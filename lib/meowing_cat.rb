4fa4cb5ed9ec1c1b77ccecb05e0c70c7662f2f4a
Adam <adamadolfo8@gmail.com>
2020-08-25 19:40:52 -0400

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,10 +1,10 @@
 ## code your solution here. 
 class Cat
     attr_accessor :name
-    def initialize()
+    def meow()
         puts "meow!"
     end
-    #notfinished ask for help when available 
+    
 
     
 
